97b6ea44e34bde7bc8af089d739049c23f9e909a
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-20 17:32:57 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Footer from "./Components/Shared/footer/footer";
+import Navbar from "./Components/Shared/Header/Navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Footer></Footer>
     </div>
   );
