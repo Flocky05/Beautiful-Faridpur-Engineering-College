127777c8da7e629eecbfe67f3e9e98b0043e1ea5
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-01-23 13:47:10 +0600

    update homepage

--- src/Components/Shared/Main/Main.js
+++ src/Components/Shared/Main/Main.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
+import { useEffect } from "react";
+import { Navigate, Outlet, useNavigate } from "react-router-dom";
 import Footer from "../footer/footer";
 import Navbar from "../Header/Navbar";
 
 const Main = () => {
+  const navigate = useNavigate();
+  useEffect(() => {
+    navigate("/HomePage");
+  }, []);
   return (
     <div>
-      <Navigate to={"/HomePage"} />
       <Navbar></Navbar>
       <Outlet></Outlet>
       <Footer></Footer>
